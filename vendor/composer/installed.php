9fee2c9f1108cef306148aa69cc64fea255193ea
unknown <zaapii@gmail.com>
2021-07-28 20:00:30 -0300

    Heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fe5429533ee7630fbd732bfc7aae7e5150d17668',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe5429533ee7630fbd732bfc7aae7e5150d17668',
+    ),
+  ),
+);
